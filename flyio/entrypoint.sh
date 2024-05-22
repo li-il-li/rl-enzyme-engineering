6551671194c9e3ee860948bacccd86029ad0f905
Dario Wirtz <dario.wirtz@sdsc.ethz.ch>
2024-05-22 09:16:15 +0000

    Remote dev environment on fly.io

--- flyio/entrypoint.sh
+++ flyio/entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/bash -e
 echo $AUTHORIZED_KEYS > /root/.ssh/authorized_keys
+git config --global credential.helper '!f() { sleep 1; echo "username=${GIT_USER}"; echo "password=${GIT_PASSWORD}"; }; f'
+
+{
+echo "export GIT_USER=$GIT_USER"
+echo "export GIT_PASSWORD=$GIT_PASSWORD"
+echo "export RCLONE_CONFIG=/root/projects/rl-enzyme-engineering/rclone.conf"
+} >> /root/.bashrc
+
 
 /usr/sbin/sshd
 exec "$@"
