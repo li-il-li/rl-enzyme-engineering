760504614bc166199650b516d58b31c44fd2491b
Dario Wirtz <dario.wirtz@sdsc.ethz.ch>
2024-06-01 18:44:40 +0200

    Container with all necessary dependencies

--- flyio/entrypoint.sh
+++ flyio/entrypoint.sh
@@ -6,9 +6,10 @@ git config --global credential.helper '!f() { sleep 1; echo "username=${GIT_USER
 echo "export GIT_USER=$GIT_USER"
 echo "export GIT_PASSWORD=$GIT_PASSWORD"
 echo "export RCLONE_CONFIG=/root/projects/rl-enzyme-engineering/rclone.conf"
+echo "export CUDA_HOME=/usr/local/cuda"
+echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64:/usr/local/cuda/extras/CUPTI/lib64"
+echo "export PATH=$PATH:$CUDA_HOME/bin"
 } >> /root/.bashrc
-
-source ${HOME}/conda/etc/profile.d/conda.sh
 
 /usr/sbin/sshd
 exec "$@"
