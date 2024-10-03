130ddd8ae422f1e2f12023673e1726d79ae099b4
Dario Wirtz <dario.wirtz@sdsc.ethz.ch>
2024-10-03 07:49:20 +0200

    Cleanup flyio setup

--- flyio/entrypoint.sh
+++ flyio/entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/bash -e
 echo $AUTHORIZED_KEYS > /root/.ssh/authorized_keys
 git config --global credential.helper '!f() { sleep 1; echo "username=${GIT_USER}"; echo "password=${GIT_PASSWORD}"; }; f'
+git config --global user.name "$GIT_NAME"
+git config --global user.email "$GIT_EMAIL"
 
 {
 echo "export GIT_USER=$GIT_USER"
 echo "export GIT_PASSWORD=$GIT_PASSWORD"
-echo "export RCLONE_CONFIG=/root/projects/rl-enzyme-engineering/rclone.conf"
 echo "export CUDA_HOME=/usr/local/cuda"
 echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64:/usr/local/cuda/extras/CUPTI/lib64"
 echo "export PATH=$PATH:$CUDA_HOME/bin"
