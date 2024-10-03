2a346d9fe95b6a5803b0f1fac509a87a26e90725
Dario Wirtz <dario.wirtz@sdsc.ethz.ch>
2024-10-03 13:50:23 +0200

    Add TORCH_HOME env to project, to store downloaded chkpts on mounted disk

--- flyio/entrypoint.sh
+++ flyio/entrypoint.sh
@@ -15,11 +15,15 @@ echo "export PATH=$PATH:$CUDA_HOME/bin"
 # Check if repo is already on disk and if not clone it
 REPO_URL="https://github.com/li-il-li/rl-enzyme-engineering.git"
 REPO_NAME=$(basename -s .git "$REPO_URL")
-if [ -d "/root/projects/$REPO_NAME" ]; then
+PROJECT_DIR="/root/projects/$REPO_NAME"
+if [ -d "$PROJECT_DIR" ]; then
     echo "Repository already exists!"
 else
-    git clone "$REPO_URL" "/root/projects/$REPO_NAME"
+    git clone "$REPO_URL" "$PROJECT_DIR"
 fi
+
+# Defines pytorch 'cache' location for eg. checkpoints
+echo "export TORCH_HOME=$PROJECT_DIR" >> /root/.bashrc
 
 /usr/sbin/sshd
 exec "$@"
